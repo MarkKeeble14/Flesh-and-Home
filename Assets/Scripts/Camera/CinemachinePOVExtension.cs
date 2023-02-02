fec85cd634e41f34b600fe0cd35d224484238964
Mark Keeble <markkeeble14@gmail.com>
2023-02-02 12:56:26 -0800

    merging with fps_camera

--- Assets/Scripts/Camera/CinemachinePOVExtension.cs
+++ Assets/Scripts/Camera/CinemachinePOVExtension.cs
@@ -11,28 +11,31 @@ public class CinemachinePOVExtension : CinemachineExtension
     [SerializeField]
     private float verticalSpeed = 10f;
     [SerializeField]
-    private float clampAngel = 80f;
+    private float clampAngle = 80f;
 
     private InputManager inputManager;
     private Vector3 startingRotation;
 
-    protected override void Awake() 
+    protected override void Awake()
     {
         inputManager = InputManager._Instance;
         base.Awake();
     }
-    
+
     protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
     {
-        if (vcam.Follow) {
-            if (stage == CinemachineCore.Stage.Aim) {
-                if (startingRotation == null) {
+        if (vcam.Follow)
+        {
+            if (stage == CinemachineCore.Stage.Aim)
+            {
+                if (startingRotation == null)
+                {
                     startingRotation = transform.localRotation.eulerAngles;
                 }
                 Vector2 deltaInput = inputManager.GetMouseDelta();
                 startingRotation.x += deltaInput.x * verticalSpeed * Time.deltaTime;
                 startingRotation.y += deltaInput.y * horizontalSpeed * Time.deltaTime;
-                startingRotation.y = Mathf.Clamp(startingRotation.y, -clampAngel, clampAngel);
+                startingRotation.y = Mathf.Clamp(startingRotation.y, -clampAngle, clampAngle);
                 state.RawOrientation = Quaternion.Euler(-startingRotation.y, startingRotation.x, 0f);
             }
         }
