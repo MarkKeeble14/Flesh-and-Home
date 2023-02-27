51e74529c8b8b768e6eacad4413d469a8826c5cf
Mark Keeble <markkeeble14@gmail.com>
2023-02-26 16:29:53 -0800

    pulse grenade launcher added as third unlock

---
+++ Assets/PulseGrenadeLauncherSettings.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PulseGrenadeLauncherSettings : WeaponAttachmentController
+{
+    [SerializeField] private PulseGrenadeSettings settings;
+
+    [SerializeField] private Transform grenadeSpawnPos;
+    [SerializeField] private PulseGrenade pulseGrenadePrefab;
+
+    private float fireRateTimer;
+    [SerializeField] private float fireRate;
+
+    public override void Fire(InputAction.CallbackContext ctx)
+    {
+        if (fireRateTimer > 0)
+        {
+            return;
+        }
+
+        PulseGrenade spawned = Instantiate(pulseGrenadePrefab, grenadeSpawnPos.position, Quaternion.identity);
+        spawned.Set(settings, Camera.main.transform.forward);
+
+        CrosshairManager._Instance.Spread(CrosshairType.PULSE_GRENADE_LAUNCHER, settings.crosshairSpread);
+
+        fireRateTimer = 1.0f / fireRate;
+    }
+
+    private void Update()
+    {
+        if (fireRateTimer > 0)
+        {
+            fireRateTimer -= Time.deltaTime;
+        }
+    }
+}
