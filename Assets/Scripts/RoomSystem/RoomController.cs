a0dfca47a4e6dcb777bfad78c3db80e48fcbc612
Kevin Baumann <baumannkev@gmail.com>
2023-03-02 07:25:37 -0800

    Added first draft of particles and camera pan

--- Assets/Scripts/RoomSystem/RoomController.cs
+++ Assets/Scripts/RoomSystem/RoomController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class RoomController : MonoBehaviour
 {
@@ -14,9 +16,38 @@ public class RoomController : MonoBehaviour
     private List<FeastableEntity> spawnedFeastables = new List<FeastableEntity>();
     private FeastableSpawnPointsController spawnPoints;
 
+    [SerializeField] private CinemachineVirtualCamera mainCam; //normal camera
+    [SerializeField] private CinemachineVirtualCamera virtualCam; //pan camera
+
+    [SerializeField] private Transform roomAbove;
+    
+    private bool normalCamera = true;
+    public float rotationTime = 2.0f;
+
+    private Transform originalLookAt;
+    private bool isRotating = false;
+    
+
     public void OnEnter()
     {
-        // Debug.Log(name + " Entered For the First Time");
+        Debug.Log(name + " Entered For the First Time");
+
+        if (name == "HubRoom")
+        {
+            if (normalCamera)
+            {
+                mainCam.Priority = 9;
+                virtualCam.Priority = 10;
+                StartCoroutine(RotateCameraUp());
+            }
+            else
+            {
+                mainCam.Priority = 10;
+                virtualCam.Priority = 9;
+            }
+
+            normalCamera = !normalCamera;
+        }
 
         // Get the spawn points controller for this room
         spawnPoints = GetComponentInChildren<FeastableSpawnPointsController>();
@@ -38,6 +69,37 @@ public class RoomController : MonoBehaviour
             // Start Spawning Feastables
             StartCoroutine(SpawnFeastableChunksCycle());
         }
+    }
+    IEnumerator RotateCameraUp()
+    {
+        isRotating = true;
+        
+        Quaternion originalRotation = virtualCam.transform.rotation;
+        Vector3 lookUp = new Vector3(0, 0, 1000);
+        Vector3 targetPosition = roomAbove.position + lookUp;
+        
+        // Look at the target position
+        virtualCam.LookAt = roomAbove;
+        
+        // Rotate towards the target position
+        float elapsedTime = 0.0f;
+        while (elapsedTime < rotationTime)
+        {
+            //Quaternion.LookRotation(targetPosition - virtualCam.transform.position)
+            virtualCam.transform.rotation = Quaternion.Slerp(originalRotation, Quaternion.LookRotation(targetPosition - virtualCam.transform.position), elapsedTime / rotationTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        // Reset the camera's LookAt
+        virtualCam.LookAt = originalLookAt;
+        
+        
+        
+        isRotating = false;
+
+        mainCam.Priority = 10;
+        virtualCam.Priority = 9;
     }
 
     public void OnReenter()
