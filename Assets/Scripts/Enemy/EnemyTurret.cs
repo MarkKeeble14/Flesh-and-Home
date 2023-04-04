3af8e53668855eab0b853e2c53ebf280c45fffc8
Kevin Baumann <baumannkev@gmail.com>
2023-04-03 19:51:02 -0700

    Added basic function to turret

---
+++ Assets/Scripts/Enemy/EnemyTurret.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyTurret : MonoBehaviour
+{
+    Transform Player;
+    public float dist;
+    public float maxDist;
+    public Transform head, muzzle;
+    public float fireRate, nextFire;
+    
+    private LayerMask canHit;
+    private float damage = 1.0f;
+    
+    public float range = 100f;
+    public DamageSource damageSource = DamageSource.RIFLE;
+    public LayerMask raycastLayerMask;
+    public float lineDisplayTime = 0.1f; // Duration to display the line
+    public Material lineMaterial; // Material for the LineRenderer
+
+    private LineRenderer lineRenderer;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("PlayerObject").transform;
+        // Initialize the LineRenderer
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = lineMaterial;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.enabled = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(Player.position, transform.position);
+        if (dist <= maxDist)
+        {
+            head.LookAt(Player);
+            if (Time.time >= nextFire)
+            {
+                nextFire = Time.time + 1f / fireRate;
+                shoot();
+            }
+        }
+    }
+    
+    void shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(muzzle.position, muzzle.forward, out hit, range, raycastLayerMask))
+        {
+            // Display the line
+            lineRenderer.SetPosition(0, muzzle.position);
+            lineRenderer.SetPosition(1, hit.point);
+            lineRenderer.enabled = true;
+            StartCoroutine(HideLineAfterDelay(lineDisplayTime));
+            
+            if (hit.collider.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.Damage(damage, damageSource);
+                Debug.Log("Damaged: " + damageable);
+            }
+        }
+    }
+    
+    IEnumerator HideLineAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        lineRenderer.enabled = false;
+    }
+
+    // void shoot()
+    // {
+    //     GameObject clone =  Instantiate(projectile, muzzle.position, transform.rotation);
+    //     clone.GetComponent<Rigidbody>().AddForce(head.forward * projectileSpeed);
+    //
+    //     if (clone.GetComponent<Collider>() != null)
+    //     {
+    //         
+    //         if (clone.GetComponent<Collider>().TryGetComponent(out IDamageable damageable))
+    //         {
+    //             damageable.Damage(damage, DamageSource.RIFLE);
+    //             Debug.Log("Damaged: " + damageable);
+    //         }
+    //     } 
+    //     Destroy(clone, 2);
+    // }
+    
+    // void shoot()
+    // {
+    //     GameObject clone = Instantiate(projectile, muzzle.position, transform.rotation);
+    //     clone.GetComponent<Rigidbody>().AddForce(head.forward * projectileSpeed);
+    //     Destroy(clone, 2);
+    // }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.name == "PlayerHitbox")
+        {
+            Debug.Log("hit");
+        }
+    }
+}
